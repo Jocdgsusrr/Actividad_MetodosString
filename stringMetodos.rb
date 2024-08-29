c7080130020101cfe168ed1f2242a5c4d62a1a96
Jocdgsusrr <153406623+Jocdgsusrr@users.noreply.github.com>
2024-08-29 20:08:21 +0000

    crear archivo string

---
+++ stringMetodos.rb
@@ -0,0 +1,26 @@
+
+class StringMetodos
+    # CortaNumDePalabras
+    def cortar_string(str)
+      str.split.size
+    end
+  
+    # InvierteCadena
+    def invertir_string(str)
+      str.reverse
+    end
+  
+    # Mayusculas
+    def mayus_string(str)
+      str.upcase
+    end
+  end
+
+  #Archivo al área de staging
+  #git add stringMetodos.rb
+
+  #Commit
+  #git commit -m "crear archivo string"
+
+  #Enviar a nube
+  #git push
