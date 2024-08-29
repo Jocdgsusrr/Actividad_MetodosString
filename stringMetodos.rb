2483d1acda3328c4d3198561a41591755669a9cc
Jocdgsusrr <153406623+Jocdgsusrr@users.noreply.github.com>
2024-08-29 20:50:39 +0000

    crear archivo string

--- stringMetodos.rb
+++ stringMetodos.rb
@@ -1,7 +1,7 @@
 
 class StringMetodos
-    # CortaNumDePalabras
-    def cortar_string(str)
+    # DivideCadena
+    def divide_string(str)
       str.split.size
     end
   
@@ -15,6 +15,14 @@ class StringMetodos
       str.upcase
     end
   end
+
+  #Instancia de StringMetodos
+  metodos = StringMetodos.new
+
+  # Usar los métodos y mostrar los resultados
+  puts "Número de palabras en 'Hola mundo': #{metodos.divide_string("Hola mundo")}"
+  puts "Cadena invertida de 'Hola mundo': #{metodos.invertir_string('Hola mundo')}"
+  puts "Cadena en mayúsculas de 'Hola mundo': #{metodos.mayus_string('Hola mundo')}"
 
   #Archivo al área de staging
   #git add stringMetodos.rb
